8e45e5663a1e9cbeda298c62ec71b06b93d655cc
agrimonprez <70689985+agrimonprez@users.noreply.github.com>
2020-09-29 15:55:50 +0200

    initial commit; https://itsolutionstuff.com/post/angular-10-crud-application-tutorialexample.html

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { PostModule } from './post/post.module';
+import { AdminModule } from './admin/admin.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PostModule,
+    AdminModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
